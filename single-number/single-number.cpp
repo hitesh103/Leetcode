2713412cf390531ee1dc85f9934e8db042fa039c
Hitesh Prajapati <hp5741609@gmail.com>
2023-05-05 22:12:31 +0530

    Time: 36 ms (16.93%) | Memory: 20.7 MB (7.71%) - LeetSync

--- single-number/single-number.cpp
+++ single-number/single-number.cpp
@@ -2,14 +2,21 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int n = nums.size();
-        map<int,int> freq;
+        // Using map to keep track of Frequency of element.
+        map<int,int> frequency;
 
+        // Iterating over array to add count of element to map.
         for(int i=0;i<n;i++){
-            freq[nums[i]]++;
+            frequency[nums[i]]++; //frequency[key]++
+            //here we are at key nums[i] incrementing it value by 1.
         }
 
+        // Ans variable to return.
         int ans;
-        for(auto it = freq.begin(); it!=freq.end();it++){
+
+        // Here we are cheking in whole map that if anyone's count is 1 means second value is 1 
+        //then return it's first means that element.
+        for(auto it = frequency.begin(); it!=frequency.end();it++){
             if(it->second == 1){
                 ans = it->first;
             }
