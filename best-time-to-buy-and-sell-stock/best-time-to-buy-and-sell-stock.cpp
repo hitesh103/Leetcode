e81c7cf8454995fa8a603c8edba6968cc4288f20
Hitesh Prajapati <hp5741609@gmail.com>
2023-05-06 18:15:43 +0530

    Time: 164 ms (5.13%) | Memory: 93.3 MB (90.30%) - LeetSync

--- best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -5,8 +5,10 @@ public:
         int buy_Value = INT_MAX;
         int maxProfit = 0;
 
+        // Iterate through vector and take min as buy_value and 
         for(int i=0;i<n;i++){
             buy_Value = min(buy_Value,nums[i]);
+            //  take buy_value differance to nums[i] and take its make because we want max profit
             maxProfit = max(maxProfit,nums[i]-buy_Value);
         }
         return maxProfit;
