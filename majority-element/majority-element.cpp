6f1247c454b6d3257e257db17dc40f39350ec829
Hitesh Prajapati <hp5741609@gmail.com>
2023-05-06 16:34:34 +0530

    Time: 26 ms (23.17%) | Memory: 19.5 MB (96.77%) - LeetSync

--- majority-element/majority-element.cpp
+++ majority-element/majority-element.cpp
@@ -4,12 +4,15 @@ public:
         int n =nums.size();
         int majorityCount = 0;
 
+        // Edge case if only one element then return it
         if(n == 1){
             return nums[0];
         }
 
+        // sort the vector
         sort(nums.begin(),nums.end());
 
+        // taking th countof majority and comparing it with n/2 and make count zero for other element count
         for(int i=0;i<n;i++){
             if(nums[i] == nums[i+1]){
                 majorityCount++;
