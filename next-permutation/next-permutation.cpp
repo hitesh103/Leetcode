f5945251ef73f1167598d933d2adfa81daae8154
Hitesh Prajapati <hp5741609@gmail.com>
2023-05-07 14:57:32 +0530

    Time: 7 ms (38.79%) | Memory: 12.1 MB (74.04%) - LeetSync

---
+++ next-permutation/next-permutation.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+    
+    // In-build function to create next permutation
+    next_permutation(nums.begin(),nums.end());
+}
+};
