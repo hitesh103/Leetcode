81f78c5f02c7c206ced854abe47b6eaad96c6804
Hitesh Prajapati <hp5741609@gmail.com>
2023-05-09 22:21:51 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (16.26%) - LeetSync

--- number-of-unequal-triplets-in-array/number-of-unequal-triplets-in-array.cpp
+++ number-of-unequal-triplets-in-array/number-of-unequal-triplets-in-array.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
-    int arr[101], cnt;
-    int recurse(int ind, int cn){
-        if(cn == 3) return 1;
-        if(ind == cnt) return 0;
-        return arr[ind]*recurse(ind+1, cn+1) + recurse(ind+1, cn);
-    }
+
     int unequalTriplets(vector<int>& nums) {
-        int n = nums.size();
-        map<int,int> mapi;
-        int ans = 1;
-        for(int i = 0 ; i < n ; i++){
-            mapi[nums[i]]++;
-        }
-        cnt = 0;
-        for(auto x : mapi) arr[cnt] = x.second, cnt++;
-        if(mapi.size() < 3) return 0;
-        return recurse(0, 0);
+       unordered_map<int, int> m;
+       for (int n : nums)
+          ++m[n];
+        int res = 0, left = 0, right = nums.size();
+       for (auto [n, cnt] : m) {
+         right -= cnt;
+         res += left * cnt * right;
+         left += cnt;
     }
+    return res;
+}
 };
