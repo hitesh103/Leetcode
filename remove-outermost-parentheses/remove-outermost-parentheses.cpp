4d6ae71578d905dfc0b8bf1ca5c35160b944bc3d
Hitesh Prajapati <hp5741609@gmail.com>
2023-05-13 15:21:23 +0530

    Time: 0 ms (100.00%) | Memory: 6.9 MB (19.71%) - LeetSync

---
+++ remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        int n = s.length();
+        int leftCount = 0;
+        int rightCount = 0;
+        string ans = "";
+        int idx1=0;
+        int idx2;
+
+        for(int i=0;i<n;i++){
+            // idx1 = i;
+            if(s[i] == '('){
+                leftCount++;
+            }else if(s[i] == ')'){
+                rightCount++;
+            } 
+
+            if(leftCount == rightCount){
+                for(int j=idx1+1;j<i;j++){
+            ans.push_back(s[j]);
+         }
+                idx1= i+1;
+            }
+        
+        }
+
+
+
+        return ans;
+    }
+};
