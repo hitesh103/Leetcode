9e1ef459e7d51d11c687b3ab120d17d59aef4535
Hitesh Prajapati <hp5741609@gmail.com>
2023-06-12 10:00:56 +0530

    Time: 3 ms (95.39%) | Memory: 8.4 MB (15.20%) - LeetSync

--- reverse-linked-list/reverse-linked-list.cpp
+++ reverse-linked-list/reverse-linked-list.cpp
@@ -13,6 +13,10 @@ public:
     ListNode* reverseList(ListNode* head) {
         //Iterative Solution
 
+        if(head == NULL || head->next == NULL){
+            return head;
+        }
+
         ListNode* prev = NULL;
         ListNode* curr = head;
         ListNode* forward = NULL;
