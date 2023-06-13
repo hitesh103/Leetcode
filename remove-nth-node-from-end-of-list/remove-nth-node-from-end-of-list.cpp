161b1d1ba92bd1032d2ce64cafd2075abcc83cd1
Hitesh Prajapati <hp5741609@gmail.com>
2023-06-13 09:40:46 +0530

    Time: 8 ms (16.89%) | Memory: 10.6 MB (52.49%) - LeetSync

--- remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
+++ remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -10,18 +10,6 @@
  */
 class Solution {
 public:
-
-    // Approch : 
-
-    // - This is a C++ function that merges two sorted linked lists into a new linked list.
-    // - It takes in two parameters - `list1` and `list2` - which are the heads of the original linked lists.
-    // - The function recursively merges the two lists into a single sorted linked list.
-    // - If either `list1` or `list2` is `nullptr`, the function returns the other list.
-    // - The function checks which node's value is smaller and sets `list1` to it.
-    // - It then sets `list1->next` to the result of invoking `mergeTwoLists` recursively with `list1->next` and `list2`.
-    // - The final line of the function returns `list1`, which is the merged list.
-    // - This implementation assumes that the original linked lists are already sorted in ascending order.
-
     ListNode* removeNthFromEnd(ListNode* head, int n) {
         // Create a dummy node to simplify edge cases
         ListNode* start = new ListNode();
