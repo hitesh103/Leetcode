2183147c1920e82fbca3c040d51b4e9b6dc8783c
Hitesh Prajapati <hp5741609@gmail.com>
2023-06-29 14:14:24 +0530

    Time: 0 ms (100.00%) | Memory: 5.7 MB (99.53%) - LeetSync

--- power-of-two/power-of-two.cpp
+++ power-of-two/power-of-two.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
 
-    bool isPowerOfTwo(int n) {
-        //Base Condn
-        if(n == 0){
-            return false;
+        // Now consider :-
+        // bit representation of 7  -> 0111
+        // bit representation of 8  -> 1000
+        // bitwise AND of 7 and 8 -> 0000
+        // we are gonna use this property for for all numbers which are powers of two
+        bool isPowerOfTwo(int n) {
+            if(n<=0) return false;
+            return ((n&(n-1))==0);
         }
-
-       return ((n==1) || (n%2==0 && isPowerOfTwo(n/2)));
-    }
-};
+    };
