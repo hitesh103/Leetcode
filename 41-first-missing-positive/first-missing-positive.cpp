30e42ab31743d843b4b3cb3fd5b561d8f66abf20
Hitesh Prajapati <hp5741609@gmail.com>
2023-08-10 22:25:40 +0530

    Time: 47 ms (93.49%) | Memory: 41.3 MB (34.26%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -2,45 +2,51 @@ class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
         int n = nums.size();
+        int containsOne = 0;
 
-        int contains = 0;
-
-        for(int i=0;i<n;i++){
-            if(nums[i] == 1){
-                contains++;
+        // Check if the array contains 1
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 1) {
+                containsOne = 1;
                 break;
             }
         }
 
-        if(contains == 0){
+        // If 1 is not present, the first missing positive is 1
+        if (containsOne == 0) {
             return 1;
         }
 
-        for(int i=0;i<n;i++){
-            if(nums[i] <= 0 || nums[i] > n ){
+        // Modify the array to have only positive integers from 1 to n
+        for (int i = 0; i < n; i++) {
+            if (nums[i] <= 0 || nums[i] > n) {
                 nums[i] = 1;
             }
         }
 
-        for(int i=0;i<n;i++){
-            int a = abs(nums[i]);
-            if(a==n){
+        // Use negative values to mark presence of elements
+        for (int i = 0; i < n; i++) {
+            int val = abs(nums[i]);
+
+            if (val == n) {
                 nums[0] = - abs(nums[0]);
-            }else{
-                nums[a] = - abs(nums[a]);
+            } else {
+                nums[val] = - abs(nums[val]);
             }
         }
 
-        for(int i=1;i<n;i++){
-            if(nums[i] > 0){
+        // Find the first missing positive by checking unmarked indices
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > 0) {
                 return i;
             }
         }
 
-        if(nums[0] > 0){
+        // If all indices are marked, n+1 is the first missing positive
+        if (nums[0] > 0) {
             return n;
         }
 
-        return n+1;
+        return n + 1;
     }
 };
