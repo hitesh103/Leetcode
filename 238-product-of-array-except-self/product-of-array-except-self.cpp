dadbd8827910133e97129aad97e1df8a4bfcc4d4
Hitesh Prajapati <hp5741609@gmail.com>
2023-08-13 16:29:09 +0530

    Time: 13 ms (96.99%) | Memory: 24 MB (59.58%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -2,22 +2,20 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
+        vector<int> ans(n, 1);  // Initialize the answer array with 1s.
 
-        vector<int> preffix(n, 1);  
-        vector<int> suffix(n, 1);   
-        vector<int> ans(n, 1);      
-
-        for(int i = 1; i < n; i++){
-            preffix[i] = preffix[i-1] * nums[i-1];
+        int suffix = 1;  // Initialize a variable to keep track of suffix product.
+        for (int i = 0; i < n; i++) {
+            ans[i] *= suffix;  // Update answer with suffix product so far.
+            suffix *= nums[i];  // Update suffix product for the next element.
         }
 
-        for(int i = n - 2; i >= 0; i--){
-            suffix[i] = suffix[i+1] * nums[i+1];
+        int prefix = 1;  // Initialize a variable to keep track of prefix product.
+        for (int i = n - 1; i >= 0; i--) {
+            ans[i] *= prefix;  // Update answer with prefix product so far.
+            prefix *= nums[i];  // Update prefix product for the next element.
         }
 
-        for(int i = 0; i < n; i++){
-            ans[i] = preffix[i] * suffix[i];
-        }
-        return ans;
+        return ans;  // Return the final product array.
     }
 };
