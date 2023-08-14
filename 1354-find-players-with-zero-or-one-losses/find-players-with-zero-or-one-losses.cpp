073daf846577edb41f103ad0985b4c31ea123b0c
Hitesh Prajapati <hp5741609@gmail.com>
2023-08-14 21:54:13 +0530

    Time: 719 ms (54.52%) | Memory: 180.7 MB (44.52%) - LeetSync

---
+++ 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<int>> findWinners(vector<vector<int>>& matches) {
+        map<int,int> m;
+        sort(matches.begin(),matches.end());
+        for(auto c:matches)
+        {
+            int x=c[0];
+            int y=c[1];
+            if(m.find(x)==m.end())
+            {
+                m[x]=0;
+            }
+            m[y]++;
+        }
+        vector<vector<int>> ans;
+        vector<int> zero;
+        vector<int> one;
+        for(auto x:m)
+        {
+            if(x.second==0)
+            zero.push_back(x.first);
+            else if(x.second==1)
+            one.push_back(x.first);
+        }
+       ans.push_back(zero);
+       ans.push_back(one);
+       return ans;
+        
+    }
+};
