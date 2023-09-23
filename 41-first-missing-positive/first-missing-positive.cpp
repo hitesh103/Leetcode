74236842aaa061f6d5c2278bb81f4b36d4316571
Hitesh Prajapati <hp5741609@gmail.com>
2023-09-23 23:18:19 +0530

    Time: 55 ms (43.60%) | Memory: 41.5 MB (34.54%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -1,52 +1,50 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        int n = nums.size();
+        int n  = nums.size();
         int containsOne = 0;
 
-        // Check if the array contains 1
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == 1) {
+        // Return 1 if one is not present.
+        for(int i=0;i<n;i++){
+            if(nums[i] == 1){
                 containsOne = 1;
                 break;
             }
         }
 
-        // If 1 is not present, the first missing positive is 1
-        if (containsOne == 0) {
+        if(containsOne == 0){
             return 1;
         }
 
-        // Modify the array to have only positive integers from 1 to n
-        for (int i = 0; i < n; i++) {
-            if (nums[i] <= 0 || nums[i] > n) {
+        // Marking all non positive and greater than array length to 1 ;
+        for(int i=0;i<n;i++){
+            if(nums[i] <= 0 || nums[i] > n){
                 nums[i] = 1;
             }
         }
 
-        // Use negative values to mark presence of elements
-        for (int i = 0; i < n; i++) {
-            int val = abs(nums[i]);
+        // using element as index and then marking them with negetive value 
+        for(int i=0;i<n;i++){
+            int idx = abs(nums[i]);
 
-            if (val == n) {
-                nums[0] = - abs(nums[0]);
-            } else {
-                nums[val] = - abs(nums[val]);
+            if(idx == n){  // for last we are storing it to first coz array are zero indexed and positive numbers start from the 1st, so we can use 0 idx.
+                nums[0] = -abs(nums[0]);
+            }else{
+                nums[idx] = -abs(nums[idx]);
             }
         }
+        // for(int i=0;i<n;i++){
+        //     cout<<nums[i]<<endl;
+        // }
 
-        // Find the first missing positive by checking unmarked indices
-        for (int i = 1; i < n; i++) {
-            if (nums[i] > 0) {
+        // and check if any element is greater than 0 
+        for(int i=1;i<n;i++){
+            if(nums[i] > 0){
                 return i;
             }
         }
-
-        // If all indices are marked, n+1 is the first missing positive
-        if (nums[0] > 0) {
-            return n;
-        }
-
-        return n + 1;
+        if(nums[0]>0)
+        return n;
+        return n+1;
     }
 };
