dfa97e024842f60628a181eb1168d8b072e74079
Hitesh Prajapati <hp5741609@gmail.com>
2023-11-29 01:17:56 +0530

    Time: 911 ms (15.02%) | Memory: 8.3 MB (70.52%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,47 +1,43 @@
 class Solution {
 public:
-    bool solve(int row, int col, vector<vector<char>>& board, int index, string word, int** visited) {
-        // Base case
-        if (index >= word.length()) {
+
+    bool solve(int row, int col, vector<vector<char>>& board , int index, string word,vector<vector<int>> &visited){
+
+        //base case
+        if(index >= word.length()){
             return true;
         }
 
-        if (row < 0 || row >= board.size() || col < 0 || col >= board[0].size() || visited[row][col] == 1 || board[row][col] != word[index]) {
+        if(row < 0 || row >= board.size() || col < 0 || col >= board[0].size() || visited[row][col] == 1 || board[row][col] != word[index]){
             return false;
         }
 
         visited[row][col] = 1;
 
-        if (solve(row + 1, col, board, index + 1, word, visited) ||
-            solve(row - 1, col, board, index + 1, word, visited) ||
-            solve(row, col + 1, board, index + 1, word, visited) ||
-            solve(row, col - 1, board, index + 1, word, visited)) {
-            return true;
-        }
+            if(solve(row+1,col,board,index+1,word,visited) ||
+            solve(row-1,col,board,index+1,word,visited) ||
+            solve(row,col+1,board,index+1,word,visited) ||
+            solve(row,col-1,board,index+1,word,visited)){
+                return true;
+            }
 
-        visited[row][col] = 0;
+            visited[row][col] = 0;
 
-        return false;
+            return false;
+        
     }
 
     bool exist(vector<vector<char>>& board, string word) {
-        int rows = board.size();
-        int cols = board[0].size();
-        
-        int** visited = new int*[rows];
-        for (int i = 0; i < rows; ++i) {
-            visited[i] = new int[cols];
-            fill(visited[i], visited[i] + cols, 0);
-        }
-
-        for (int row = 0; row < rows; ++row) {
-            for (int col = 0; col < cols; ++col) {
-                if (solve(row, col, board, 0, word, visited)) {
+    vector<vector<int>> visited(board.size(), vector<int>(board[0].size(), 0));
+        for(int row = 0;row <board.size();row++){
+            for(int col = 0;col < board[0].size();col++){
+                if(solve(row,col,board,0,word,visited)){
                     return true;
                 }
             }
         }
 
         return false;
+
     }
 };
