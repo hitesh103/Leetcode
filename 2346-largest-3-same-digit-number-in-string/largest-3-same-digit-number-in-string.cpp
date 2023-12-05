260e9949e1507d841583fbf3794752ea6a1ebbd7
Hitesh Prajapati <hp5741609@gmail.com>
2023-12-05 10:16:32 +0530

    Time: 0 ms (100.00%) | Memory: 7.1 MB (18.82%) - LeetSync

--- 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.cpp
+++ 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.cpp
@@ -3,36 +3,31 @@ public:
     string largestGoodInteger(string num) {
 
         int n = num.length();
-        vector<int> maap(10, 0);
-
         int count = 1;
+        int maxDigit = -1;
 
-        for(int i=0;i<n;i++){
-            if(num[i] == num[i+1]){
+        for (int i = 0; i < n ; i++) {
+            if (num[i] == num[i + 1]) {
                 count++;
-            }else if(num[i] != num[i+1]){
-                if(count >= 3){
-                    maap[num[i] - '0'] += count;
+            } else {
+                if (count >= 3 && (num[i] - '0') > maxDigit) {
+                    maxDigit = num[i] - '0';
                 }
                 count = 1;
             }
         }
 
-        if (count >= 3) {
-            maap[num[n - 1] - '0'] += count;
+
+
+        if (maxDigit == -1) {
+            return "";
         }
 
-        for(int i = 9; i >= 0; i--){
-            if(maap[i] != 0){
-                if(i == 0){
-                    return "000";
-                }
-                int ans = 111 * i;
-                return to_string(ans);
-            }
+        if (maxDigit == 0) {
+            return "000";
         }
 
-        return "";
+        return to_string(111 * maxDigit);
 
     }
 };
