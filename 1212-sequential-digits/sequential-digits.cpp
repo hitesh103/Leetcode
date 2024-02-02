b2aeaf6bb966c2c354ea554006249b361ed3bcab
Hitesh Prajapati <hp5741609@gmail.com>
2024-02-02 21:37:38 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (9.62%) - LeetSync

--- 1212-sequential-digits/sequential-digits.cpp
+++ 1212-sequential-digits/sequential-digits.cpp
@@ -4,17 +4,9 @@ public:
         vector<int> ans;
         for(int i=1;i<=9;i++){
             long long num = i;
-            // cout << "num : " << num << " i : " << i << endl;
-            // if(num >= low && num <= high){
-            //     cout << "In range 1 added : " << num << endl;
-            //     ans.push_back(num);
-            // }
             for(int j=i+1;j<=9;j++){
-                // cout << "j : " << j <<endl;
                 num = num*10 + j;
-                // cout << "NUM : " << num << endl;
                 if(num >= low && num <= high){
-                    // cout << "In range 2 added : " << num << endl;
                     ans.push_back(num);
                 }
             }
