fb6500db6174fc33d9329165b92b681a272e004e
Hitesh Prajapati <hp5741609@gmail.com>
2024-02-02 21:37:15 +0530

    Time: 2 ms (43.93%) | Memory: 7.3 MB (13.81%) - LeetSync

--- 1212-sequential-digits/sequential-digits.cpp
+++ 1212-sequential-digits/sequential-digits.cpp
@@ -4,13 +4,18 @@ public:
         vector<int> ans;
         for(int i=1;i<=9;i++){
             long long num = i;
-            if(num >= low && num <= high){
-                ans.push_back(num);
-            }
+            // cout << "num : " << num << " i : " << i << endl;
+            // if(num >= low && num <= high){
+            //     cout << "In range 1 added : " << num << endl;
+            //     ans.push_back(num);
+            // }
             for(int j=i+1;j<=9;j++){
-                 num = num*10 + j;
+                // cout << "j : " << j <<endl;
+                num = num*10 + j;
+                // cout << "NUM : " << num << endl;
                 if(num >= low && num <= high){
-                ans.push_back(num);
+                    // cout << "In range 2 added : " << num << endl;
+                    ans.push_back(num);
                 }
             }
         }
