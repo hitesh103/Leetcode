037b90a9db6adb6f61b4988a0050ccaa395ba58d
Hitesh Prajapati <hp5741609@gmail.com>
2024-02-06 21:51:04 +0530

    Time: 29 ms (65.26%) | Memory: 24.4 MB (20.94%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        int n = strs.size();  // Use .size() for vector length
-        map<string, vector<string>> mpp;
-
-        // Group anagrams in the map
-        for (int i = 0; i < n; i++) {
-            string s = strs[i];
-            sort(s.begin(), s.end());
-
-            // Insert or append the current string to the anagram group
-            mpp[s].push_back(strs[i]);
+        int n = strs.size();
+        unordered_map<string,vector<string>> anagrams;
+        
+        for(int i=0;i<n;i++){
+            string temp_s = strs[i];
+            sort(temp_s.begin(),temp_s.end());
+            anagrams[temp_s].push_back(strs[i]);
         }
 
-        // Extract the grouped anagrams from the map
-        vector<vector<string>> result;
-        for (const auto& pair : mpp) {
-            result.push_back(pair.second);
+        vector<vector<string>> ans;
+
+        for(auto i : anagrams){
+            ans.push_back(i.second);
         }
 
-        return result;
+        return ans;
+
     }
 };
