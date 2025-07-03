4ee470ec84eb7376231521e982defb307431e7a4
Hitesh Prajapati <hp5741609@gmail.com>
2025-07-04 00:10:34 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (26.75%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,9 +1,20 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        if(s==t) return true;
-        return false;
+        unordered_map<char,int> mp;
+
+        for (char c : s){
+            mp[c]++;
+        }
+
+        for(char d : t){
+            mp[d]--;
+        }
+
+        for(auto it : mp){
+            if(it.second != 0) return false;
+        }
+
+        return true;
     }
 };
