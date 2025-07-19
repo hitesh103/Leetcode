b92e65e7225066a27a39233e285eb7e3304bbf46
Hitesh Prajapati <hp5741609@gmail.com>
2025-07-19 22:03:52 +0530

    Time: 923 ms (69.41%) | Memory: 0B (100.00%) - LeetSync

--- 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
+++ 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -1,12 +1,5 @@
-WITH ContestRegistrations AS (
-    SELECT contest_id, COUNT(DISTINCT user_id) AS registered_users
-    FROM Register
-    GROUP BY contest_id
-)
 SELECT 
-    cr.contest_id,
-    ROUND((cr.registered_users / (SELECT COUNT(*) FROM Users)) * 100, 2) AS percentage
-FROM 
-    ContestRegistrations cr
-ORDER BY 
-    percentage DESC, contest_id ASC;
+    contest_id,round(count(DISTINCT user_id) * 100 /(SELECT count(user_id) FROM Users) ,2) as percentage
+FROM Register
+GROUP BY contest_id
+ORDER BY percentage DESC,contest_id
